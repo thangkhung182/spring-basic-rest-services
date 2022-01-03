e8dd9b315ba61f2776ca7812f0ac607581351ed4
Trung Nguyen <thangkhung156@gmail.com>
2022-01-03 14:38:49 +0700

    finish test persistence layer

--- microservices/recommendation-service/src/main/java/com/trungnguyen/microservices/core/recommendation/services/RecommendationServiceImpl.java
+++ microservices/recommendation-service/src/main/java/com/trungnguyen/microservices/core/recommendation/services/RecommendationServiceImpl.java
@@ -8,12 +8,9 @@ import com.trungnguyen.microservices.core.recommendation.persistence.Recommendat
 import com.trungnguyen.util.http.ServiceUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 
