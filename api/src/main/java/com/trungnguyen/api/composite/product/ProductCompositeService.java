75a515ca50bd679d9cdce5db6c55b759cc028e5e
Trung Nguyen <thangkhung156@gmail.com>
2022-01-03 15:20:55 +0700

    config open api

--- api/src/main/java/com/trungnguyen/api/composite/product/ProductCompositeService.java
+++ api/src/main/java/com/trungnguyen/api/composite/product/ProductCompositeService.java
@@ -2,22 +2,48 @@ package com.trungnguyen.api.composite.product;
 
 import com.trungnguyen.api.model.dto.ProductAggregate;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "ProductComposite", description = "REST API for composite product information.")
 public interface ProductCompositeService {
 
+    @Operation(
+            summary = "${api.product-composite.create-composite-product.description}",
+            description = "${api.product-composite.create-composite-product.notes}")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "400", description = "${api.responseCodes.badRequest.description}"),
+            @ApiResponse(responseCode = "422", description = "${api.responseCodes.unprocessableEntity.description}")
+    })
     @PostMapping(
             value = "/product-composite",
             consumes = "application/json")
     void createProduct(@RequestBody ProductAggregate body);
 
+    @Operation(
+            summary = "${api.product-composite.get-composite-product.description}",
+            description = "${api.product-composite.get-composite-product.notes}")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "${api.responseCodes.ok.description}"),
+            @ApiResponse(responseCode = "400", description = "${api.responseCodes.badRequest.description}"),
+            @ApiResponse(responseCode = "404", description = "${api.responseCodes.notFound.description}"),
+            @ApiResponse(responseCode = "422", description = "${api.responseCodes.unprocessableEntity.description}")
+    })
     @GetMapping(
             value = "/product-composite/{productId}",
             produces = "application/json")
     ProductAggregate getProduct(@PathVariable int productId);
 
+    @Operation(
+            summary = "${api.product-composite.delete-composite-product.description}",
+            description = "${api.product-composite.delete-composite-product.notes}")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "400", description = "${api.responseCodes.badRequest.description}"),
+            @ApiResponse(responseCode = "422", description = "${api.responseCodes.unprocessableEntity.description}")
+    })
     @DeleteMapping(value = "/product-composite/{productId}")
     void deleteProduct(@PathVariable int productId);
 }
