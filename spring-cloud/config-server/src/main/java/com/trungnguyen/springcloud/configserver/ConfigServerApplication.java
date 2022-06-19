09621cd45f915adf3108a685ad754575e0eeaea1
Trung Nguyen <thangkhung156@gmail.com>
2022-06-19 16:31:24 +0700

    upgrade spring boot 2.7

---
+++ spring-cloud/config-server/src/main/java/com/trungnguyen/springcloud/configserver/ConfigServerApplication.java
@@ -0,0 +1,9 @@
+package com.trungnguyen.springcloud.configserver;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+}
--- spring-cloud/eureka-server/src/main/java/com/trungnguyen/springcloud/eurekaserver/SecurityConfig.java
+++ spring-cloud/eureka-server/src/main/java/com/trungnguyen/springcloud/eurekaserver/SecurityConfig.java
@@ -2,16 +2,20 @@ package com.trungnguyen.springcloud.eurekaserver;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 import org.w3c.dom.Node;
 
 @Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
     private final String username;
 
     private final String password;
@@ -25,16 +29,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         this.password = password;
     }
 
-    @Override
-    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .passwordEncoder(NoOpPasswordEncoder.getInstance())
-                .withUser(username).password(password)
-                .authorities("USER");
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        var user = User.withDefaultPasswordEncoder()
+                .username(username)
+                .password(password)
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 // Disable CRCF to allow services to register themselves with Eureka
                 .csrf()
@@ -43,5 +49,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic();
+
+        return http.build();
     }
 }
